85c5203834281f29b687096cf3aa6d007faa9515
Luis <lfmnovaes@gmail.com>
2021-11-30 23:36:10 -0300

    Fill up queries.sql with answer from the exercise

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-/* Populate database with sample data */
+/* Populate database with sample data - part 1 */
 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Agumon', '2020-02-03', '10.2', TRUE, '0');
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Gabumon', '2018-11-15', '8', TRUE, '2');
